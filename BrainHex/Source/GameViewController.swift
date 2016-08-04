f14a9434287215cce7e59a1d86c33259b446580c
Gaurav <gaurav.keshre+bbkt@goodbox.in>
2016-08-05 02:42:58 +0530

    basic game ready. need to integrate online images now

--- BrainHex/Source/GameViewController.swift
+++ BrainHex/Source/GameViewController.swift
@@ -8,14 +8,57 @@
 
 import UIKit
 
+enum GameMode{
+    case observing
+    case guessing
+    case ended
+}
+
 class GameViewController: UIViewController {
     
-    @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var lblTimer: UILabel!
     
-    private var imageArray =  [GameImage]()
+    @IBOutlet weak var lblTimer     : UILabel!
+    @IBOutlet weak var btnReplay    : UIButton!
+    @IBOutlet weak var imgHint      : UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet var bottomBarHeightConstraint: NSLayoutConstraint! /// need to be strong 💪🏼
+    
+    
     private var timer: NSTimer?
     private var numberOfTicks: NSTimeInterval = 0
+    
+    private var randomImage: GameImage!
+    private var gameMode: GameMode = .observing{
+        didSet{
+            self.btnReplay?.hidden = (self.gameMode != .ended)
+            self.lblTimer?.hidden = (self.gameMode != .observing)
+            self.imgHint?.hidden = (self.gameMode != .guessing)
+            switch self.gameMode {
+            case .observing:
+                bottomBarHeightConstraint.active = true
+            case .guessing:
+                let randomNum = random() % GameConstants.MatrixSize
+                print("Random Index = \(randomNum)")
+                self.randomImage = self.imageArray[randomNum]
+                self.imgHint.image = UIImage(named: self.randomImage.pathOrName)
+                bottomBarHeightConstraint.active = false
+            case .ended:
+                bottomBarHeightConstraint.active = true
+            }
+            UIView.animateWithDuration(0.3, animations: {
+                self.view.layoutSubviews()
+            })
+        }
+    }
+    
+    private var imageArray   =  [GameImage]()
+    private var guessedIPs   =  [NSIndexPath](){
+        didSet{
+            if self.imageArray.count > 0 && self.guessedIPs.count == imageArray.count{
+                self.gameMode = .ended
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +73,11 @@ class GameViewController: UIViewController {
     
     //MARK:- IBAction methods
     @IBAction func handleRefreshButton(sender: AnyObject) {
+        self.lblTimer?.text = "\(Int(GameConstants.ObservationTime))"
+        self.numberOfTicks = 0
+        self.closeAllTiles()
+        self.gameMode = .observing
+        self.guessedIPs.removeAll()
         self.startTimer()
     }
 }
@@ -51,6 +99,10 @@ extension GameViewController: UICollectionViewDataSource, UICollectionViewDelega
     }
     
     func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        guard !guessedIPs.contains(indexPath) else{
+            return false
+        }
+        
         guard let cell = self.collectionView.cellForItemAtIndexPath(indexPath) as? GameTileCell else{
             return false // let it go
         }
@@ -70,6 +122,23 @@ extension GameViewController: UICollectionViewDataSource, UICollectionViewDelega
         ///Flip Our guy
         cell.flipCardTo(.open);
         
+        ///Check if win
+        
+        if self.randomImage.uuid == cell.gameData.uuid{
+            print("WIN !!")
+            self.gameMode = .ended
+            let score = GameConstants.MatrixSize - self.guessedIPs.count
+            let alert = UIAlertController(title: "Congratulations! You Won 🎉", message: "Yes this is the right match. Your score is \(score) Points", preferredStyle: .Alert)
+            
+            let action = UIAlertAction(title: "Replay", style: .Default, handler: { (action) in
+                self.handleRefreshButton(action)
+            })
+            alert.addAction(action)
+            
+            self.presentViewController(alert, animated: true, completion:nil)        }
+        
+        
+        self.guessedIPs.append(indexPath)
     }
     
     //MARK:- Convenience methods
@@ -87,9 +156,11 @@ extension GameViewController: GameTileCellDelegate{
     func gameCell(cell: GameTileCell, willToggleStateTo state: GameTileState){
         
     }
+    
     func gameCellBeginFlipAnimation(cell: GameTileCell){
         self.collectionView.userInteractionEnabled = false
     }
+    
     func gameCellEndFlipAnimation(cell: GameTileCell){
         self.collectionView.userInteractionEnabled = true
     }
@@ -103,17 +174,12 @@ extension GameViewController{
         guard self.timer == nil else{
             return // timer is running
         }
-        
-        self.lblTimer?.text = "\(Int(GameConstants.ObservationTime))"
-        self.lblTimer?.hidden = false
         timer = NSTimer(timeInterval: 1,
                         target: self,
                         selector: #selector(GameViewController.timerTick(_:)),
                         userInfo: nil, repeats: true)
         
         NSRunLoop.currentRunLoop().addTimer(timer!, forMode: NSDefaultRunLoopMode)
-        
-        //timer?.fire()
     }
     
     private func stopTimer(){
@@ -125,13 +191,10 @@ extension GameViewController{
         guard numberOfTicks < GameConstants.ObservationTime - 1 else{
             self.stopTimer()
             self.closeAllTiles()
-            self.lblTimer?.hidden = true
+            self.gameMode = .guessing
             return
         }
         self.numberOfTicks += 1
         self.lblTimer?.text = "\(Int(GameConstants.ObservationTime - self.numberOfTicks))"
-        
     }
-    
-    
 }
